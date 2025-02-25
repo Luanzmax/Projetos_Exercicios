715420c40d24cc2138c2134cb4d97bd309132ab0
Luanzmax <luandesouzaroque@gmail.com>
2025-02-25 17:09:12 +0000

    Protótipo do Super Trunfo no meu próprio respositório

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(){
+    //Construção das variáveis
+    char estado;
+    char cod_estado[3];
+    char nome_cidade[30];
+    int populacao, num_pontosTuristico;
+    float Area_km, PIB;
+
+    //Usando prinf e scanf para construir a interação com o usuário
+
+
+
+}
